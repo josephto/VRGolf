d234ef721e1ca5a6d8ca831e93324fdc3b2a0334
josephto <joccerT25@gmail.com>
2015-03-26 23:30:42 -0400

    golf club and ball collision and physics implemtnation

---
+++ VRGolf/Assets/GolfBall.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GolfBall : MonoBehaviour {
+
+	public bool cameraFollow = false;
+	private float distanceFromBall = 50f;
+	public Transform flagPos;
+	private Camera mainCamera;
+	private SphereCollider golfBallCollider;
+
+	// Use this for initialization
+	void Start () {
+		mainCamera = Camera.main;
+		golfBallCollider = this.gameObject.GetComponent<SphereCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (cameraFollow) {
+
+			//make collider size of ball
+			golfBallCollider.radius = transform.localScale.x/2.0f;
+			golfBallCollider.center = new Vector3(0,0,0);
+
+			Vector3 dir = (this.gameObject.transform.position+new Vector3(0,2,0) - flagPos.position).normalized;
+			mainCamera.transform.position = this.gameObject.transform.position +new Vector3(0,02) + dir*distanceFromBall;
+
+			//toa
+			float angle = Mathf.Atan(2/(this.gameObject.transform.position - flagPos.position).magnitude);
+
+			mainCamera.transform.LookAt (flagPos);
+		}else{
+
+			//make collider larger
+			golfBallCollider.radius = transform.localScale.x;
+			golfBallCollider.center = new Vector3(0,transform.localScale.x/2.0f,0);
+		}
+		Debug.LogError ("golfball velocity: "+this.gameObject.rigidbody.velocity);
+	}
+}
--- VRGolf/Assets/GolfClub.cs
+++ VRGolf/Assets/GolfClub.cs
@@ -5,37 +5,57 @@ public class GolfClub : MonoBehaviour {
 
 	private Vector3 oldPosition;
 	private Vector3 velocity;
+	private bool contact = false;
 
 	// Use this for initialization
 	void Start () {
 		oldPosition = transform.position;
 	}
 
-	void OnTriggerEnter(Collider other){
-		if(other.gameObject.name == "Sphere"){
-			Debug.LogError ("Enter "+velocity);
-			other.gameObject.rigidbody.velocity = velocity*2f;
-		}
-	}
+//	void OnTriggerEnter(Collider other){
+//		if(other.gameObject.name == "Sphere"){
+//			Debug.LogError ("Enter "+velocity);
+//			other.gameObject.rigidbody.velocity = velocity;
+//		}
+//	}
 
-	void OnTriggerStay(Collider other){
-		if(other.gameObject.name == "Sphere"){
-			Debug.LogError ("Stay "+velocity);
-			other.gameObject.rigidbody.velocity = velocity*2f;
-		}
-	}
-
-	void OnTriggerExit(Collider other){
-		if(other.gameObject.name == "Sphere"){
-			Debug.LogError ("Exit "+velocity);
-			other.gameObject.rigidbody.velocity = velocity*2f;
-		}
-	}
+//	void OnTriggerStay(Collider other){
+//		if(other.gameObject.name == "Sphere"){
+//			Debug.LogError ("Stay "+velocity);
+//			other.gameObject.rigidbody.velocity = velocity*2f;
+//		}
+//	}
+//
+//	void OnTriggerExit(Collider other){
+//		if(other.gameObject.name == "Sphere"){
+//			Debug.LogError ("Exit "+velocity);
+//			other.gameObject.rigidbody.velocity = velocity*2f;
+//		}
+//	}
 
 	// Update is called once per frame
 	void Update () {
+	}
+
+	void FixedUpdate(){
 		Vector3 newPosition = transform.position;
 		velocity = (newPosition - oldPosition) / Time.deltaTime;
+		if (!contact){
+			Vector3 origin = newPosition;
+			Vector3 direction = (oldPosition-origin).normalized;
+			RaycastHit hit = new RaycastHit();
+			if(Physics.Raycast(origin, direction, out hit) && velocity.magnitude > 1000f ){ //threshold for swing arbitrary
+				if (hit.collider.name == "GolfBall"){
+					Debug.LogError ("velocity in FixedUpdate: "+velocity/15f);
+					hit.collider.gameObject.rigidbody.velocity = velocity/15f;
+					contact = true;
+					GolfBall golfBall = hit.collider.gameObject.GetComponent<GolfBall>();
+					if (golfBall != null){
+						golfBall.cameraFollow = true;
+					}
+				}
+			}
+		}
 		oldPosition = newPosition;
 	}
 }
